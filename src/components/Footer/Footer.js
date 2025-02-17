cc77ff3710d003a678b6f81e2809fc5c3da64129
giapdq04 <quanggiap04@gmail.com>
2025-02-17 21:42:40 +0700

    Add Footer, Modal components and styles

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div style={{
+            color: 'red',
+        }}>
+            Footer
+        </div>
+    );
+};
+
+export default Footer;
