548e6dd2330f245256ceab93a0b40de66282e2c0
giapdq04 <quanggiap04@gmail.com>
2025-02-27 10:02:53 +0700

    fix modal successfully

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,31 +1,34 @@
 import {faXmark} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import classNames from "classnames/bind";
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Modal.module.scss';
 
 const cx = classNames.bind(styles);
 
-const Modal = ({ children, isShow, onClose, width }) => {
+const Modal = ({children, isShow, onClose, width}) => {
     const [isClosing, setIsClosing] = useState(false);
+
+    useEffect(() => {
+        setIsClosing(false);
+    }, [])
+
 
     const handleClose = () => {
         setIsClosing(true);
         setTimeout(() => {
-            setIsClosing(false);
             onClose();
+            setIsClosing(false);
         }, 250);
     }
 
     if (!isShow && !isClosing) return null;
 
     return (
-        <div className={cx('wrapper', 
-        { 'fade-out': isClosing }
-        )} onClick={handleClose}>
-            <div className={cx('container')} style={width ? { width: width } : {}} onClick={(e) => e.stopPropagation()}>
+        <div className={cx('wrapper', 'show', isClosing ? 'fade-out' : 'fade-in')} onClick={handleClose}>
+            <div className={cx('container')} style={width ? {width: width} : {}} >
                 <button className={cx('close-button')} onClick={handleClose}>
-                    <FontAwesomeIcon icon={faXmark} />
+                    <FontAwesomeIcon icon={faXmark}/>
                 </button>
                 {children}
             </div>
