4b051ad7922a88b0fdf86ca276a4d60d292bb3c2
singa <phamvietanh19218@gmail.com>
2025-03-24 13:34:46 +0700

    slide donedone

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,14 +1,15 @@
-// components/Slider/Slider.js
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import classNames from "classnames/bind";
-import React, { useState } from "react";
-import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css";
-import styles from "./Slider.module.scss";
-import { useSelector } from "react-redux";
+"use client"
 
-const cx = classNames.bind(styles);
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import classNames from "classnames/bind"
+import { useState } from "react"
+import { Slide } from "react-slideshow-image"
+import "react-slideshow-image/dist/styles.css"
+import styles from "./Slider.module.scss"
+import { useSelector } from "react-redux"
+
+const cx = classNames.bind(styles)
 
 const Slideshow = ({
   autoplay = true,
@@ -19,37 +20,33 @@ const Slideshow = ({
   nextArrowCustom,
   prevArrowCustom,
 }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const slides = useSelector((state) => state.slides.slides); // Lấy slides từ store
-  console.log("Slides in Slideshow:", slides); // Kiểm tra slides trong component
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const slides = useSelector((state) => state.slides.slides) // Lấy slides từ store
+  console.log("Slides in Slideshow:", slides) // Kiểm tra slides trong component
 
   const nextArrow = (
     <button className={cx("arrow", "right")}>
       <FontAwesomeIcon icon={faChevronRight} />
     </button>
-  );
+  )
 
   const prevArrow = (
     <button className={cx("arrow", "left")}>
       <FontAwesomeIcon icon={faChevronLeft} />
     </button>
-  );
+  )
 
   return (
-    <div className="slide-container">
+    <div className={cx("slide-container")}>
       <Slide
         cssClass={cx("slide-show")}
         onChange={(from, to) => {
-          setCurrentIndex(to);
+          setCurrentIndex(to)
         }}
         arrows={true}
         nextArrow={nextArrowCustom ?? nextArrow}
         prevArrow={prevArrowCustom ?? prevArrow}
-        indicators={(index) => (
-          <span
-            className={cx("indicator", { active: currentIndex === index })}
-          ></span>
-        )}
+        indicators={(index) => <span className={cx("indicator", { active: currentIndex === index })}></span>}
         autoplay={autoplay}
         infinite={infinite}
         duration={duration}
@@ -59,23 +56,22 @@ const Slideshow = ({
       >
         {slides && slides.length > 0 ? (
           slides.map((slide) => (
-            <div key={slide._id} className="each-slide">
+            <div key={slide._id} className={cx("each-slide")}>
               <div
+                className={cx("slide-image")}
                 style={{
                   backgroundImage: `url(${slide.imageUrl})`,
-                  height: "400px", // Đảm bảo chiều cao cố định
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
                 }}
-              />
+              ></div>
             </div>
           ))
         ) : (
-          <div>Không có slide để hiển thị</div>
+          <div className={cx("no-slides")}>Không có slide để hiển thị</div>
         )}
       </Slide>
     </div>
-  );
-};
+  )
+}
 
-export default Slideshow;
+export default Slideshow
+
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,48 +1,23 @@
-// pages/Home/Home.js
-import React, { memo } from "react";
-import classNames from "classnames/bind";
-import { useSelector } from "react-redux";
-import Slideshow from "../../components/Slider/Slider";
-import List from "./List/List";
-import config from "../../config";
-import styles from "./Home.module.scss";
+import { memo } from "react"
+import classNames from "classnames/bind"
+import { useSelector } from "react-redux"
+import Slideshow from "../../components/Slider/Slider"
+import List from "./List/List"
+import config from "../../config"
+import styles from "./Home.module.scss"
 
-const cx = classNames.bind(styles);
+const cx = classNames.bind(styles)
 
-// Component con cho Slide Item - Memo hóa
-const SlideItem = memo(({ slideImage }) => (
-  <div>
-    <div
-      className={cx("slide")}
-      style={{
-        backgroundImage: `url(${slideImage.imageUrl})`,
-        height: "400px", // Đảm bảo chiều cao cố định
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    />
-  </div>
-));
-
-// Component chính - Memo hóa
 const Home = memo(() => {
-  const courses = useSelector((state) => state.courses);
-  const slides = useSelector((state) => state.slides.slides); // Lấy slides từ store
-  console.log("Slides in Home:", slides); // Kiểm tra slides trong Home
+  const courses = useSelector((state) => state.courses)
+  const slides = useSelector((state) => state.slides.slides) // Lấy slides từ store
+  console.log("Slides in Home:", slides) // Kiểm tra slides trong Home
 
   return (
     <div className={cx("wrapper")}>
       <div className={cx("container")}>
-        <div className={cx("slide-container")}>
-          <Slideshow>
-            {slides && slides.length > 0 ? (
-              slides.map((slideImage) => (
-                <SlideItem key={slideImage._id} slideImage={slideImage} />
-              ))
-            ) : (
-              <div>Không có slide để hiển thị</div>
-            )}
-          </Slideshow>
+        <div className={cx("slide-wrapper")}>
+          <Slideshow />
         </div>
 
         <div className={cx("content-wrapper")}>
@@ -56,7 +31,8 @@ const Home = memo(() => {
         </div>
       </div>
     </div>
-  );
-});
+  )
+})
 
-export default Home;
+export default Home
+
